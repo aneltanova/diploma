c4c337ecfab8be19097982b0c1683daff5de34b9
Adam_emes <eltanovaanel.0308@gmail.com>
2022-04-12 16:29:08 +0600

    Initial commit

---
+++ diploma/src/test/java/iitu/kz/diploma/DiplomaApplicationTests.java
@@ -0,0 +1,13 @@
+package iitu.kz.diploma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiplomaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
