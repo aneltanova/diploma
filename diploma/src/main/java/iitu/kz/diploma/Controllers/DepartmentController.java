d04c1a1418e5e5a96944ad415feab7f49ebcdc4c
Adam_emes <eltanovaanel.0308@gmail.com>
2022-04-17 18:13:57 +0600

    controller added

--- diploma/src/main/java/iitu/kz/diploma/Controllers/DepartmentController.java
+++ diploma/src/main/java/iitu/kz/diploma/Controllers/DepartmentController.java
@@ -1,4 +1,60 @@
 package iitu.kz.diploma.Controllers;
 
+import iitu.kz.diploma.Models.Departments;
+//import iitu.kz.diploma.Service.DepartmentService;
+import iitu.kz.diploma.Repos.DepartmentsRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+//@RequestMapping("/departments")
 public class DepartmentController {
+
+//    @Autowired
+//    private DepartmentsRepo departmentsRepo;
+
+    private final DepartmentsRepo departmentsRepo;
+
+    @Autowired
+    DepartmentController(DepartmentsRepo departmentsRepo){
+        this.departmentsRepo = departmentsRepo;
+    }
+
+    @GetMapping("/find-all")
+    public List<Departments> findAll() {
+        return (List<Departments>) departmentsRepo.findAll();
+    }
+
+    @PostMapping("/create-department")
+    public Departments add(@RequestBody Departments department) {
+        departmentsRepo.save(department);
+        return department;
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Departments> find(@PathVariable("id") long id) {
+        return departmentsRepo.findById(id);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void delete(@PathVariable("id") long id) {
+        departmentsRepo.deleteById(id);
+    }
+
+    @PutMapping("/update/{id}")
+    public Departments update(@PathVariable("id") long id, @RequestBody Departments department) {
+        department.setId(id);
+        departmentsRepo.save(department);
+        return department;
+    }
 }
+
+//{
+//    "department":"MCM",
+//    "faculty":"FCTC",
+//    "head_of_dep":"Ydyrys A. Zh."
+//}
---
+++ diploma/src/main/java/iitu/kz/diploma/Repos/LoadRepo.java
@@ -0,0 +1,7 @@
+package iitu.kz.diploma.Repos;
+
+import iitu.kz.diploma.Models.Load;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LoadRepo extends JpaRepository<Load, Long> {
+}
